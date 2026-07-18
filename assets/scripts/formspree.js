aaa54ec1b3ac933714d361bacf1439e7e4b73053
“teeteate” <hiretamara@protonmail.com>
2026-07-18 15:57:35 -0400

    fix  portfolio form

---
+++ assets/scripts/formspree.js
@@ -0,0 +1,9 @@
+  window.formspree =
+    window.formspree ||
+    function () {
+      (formspree.q = formspree.q || []).push(arguments);
+    };
+  formspree("initForm", {
+    formElement: "#my-form",
+    formId: "xgogkzzw",
+  });
