555ee797c9fcd75755d0aed32ab234f1ce85159f
“teeteate” <306310134+hiretamara@users.noreply.github.com>
2026-07-18 22:36:05 -0400

    fix code typos

--- portfolio-projects/otterly-funny/scripts/server.js
+++ portfolio-projects/otterly-funny/scripts/server.js
@@ -144,11 +144,8 @@ app.get('*any', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running online on port ${PORT}`);
-});
+ 
 
-
-app.listen(PORT, () => {
+app.listen(PORT,  '0.0.0.0',() => {
     console.log(`Server is running on port ${PORT}`);
 });
